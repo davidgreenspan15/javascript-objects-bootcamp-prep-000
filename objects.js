6c9acdfb7b4f4cb49ba7cfe059816db78b080732
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-02-28 14:56:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,5 +4,5 @@ function updatePlaylist(playlist,artistName,songTitle){
 playlist[artistName] = songTitle
 }
 function removeFromPlaylist(playlist,artistName){
-playlist[artistName] = songTitle
+delete playlist.artistName
 }
